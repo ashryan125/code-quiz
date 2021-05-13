4aedd767dcb73b7d9c7f4953fb566078ddb266e8
ashryan <20115553+ashryan125@users.noreply.github.com>
2021-05-13 18:23:21 -0400

    added timer countdown

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,57 @@
+var questions = [
+     {
+          title: "Commonly used data types DO NOT Include:",
+          choices: ["strings", "booleans", "alerts", "numbers"],
+          answer: "alerts"
+     },
+     {
+          title: "The condition in an if / else statement is enclosed with ________.",
+          choices: ["quotes", "curly brackets", "parenthesis", "square brackets"],
+          answer: "curly brackets"
+     },
+     {
+          title: "Arrays in JavaScript can be used to store ________.",
+          choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+          answer: "all of the above"
+     },
+     {
+          title: "String values must be enclosed within ________ when being assigned to variables" ,
+          choices: ["commas", "curly brackets", "quotes", "parenthesis"],
+          answer: "quotes"
+     },
+     {
+          title: "A very useful tool used during development and debugging for printing content to the debugger is:" ,
+          choices: ["JavaScript", "terminal/bash", "for loops", "console.log"],
+          answer: "console.log"
+     },
+]
+
 var buttonEl = document.querySelector("#quiz-btn");
 
-
-// when user clicks sumbit
-
-buttonEl.addEventListener("click", function() {
-     alert("button clicked");
-});
-
+var score = 0;
+var questionNum = 0;
+var penalty = 10;
 // set initial timer to 75sec and first question
-
 var timer = 75;
+
 var countdown = function() {
      timer--;
-     if(timer === 0){
+     if(timer === 0) {
           clearInterval(startCountdown);
-     }
-};
+     };
+     console.log(timer);
+}
 
 var startCountdown = setInterval(countdown, 1000);
+// when user clicks sumbit, start countdown timer and display first question
+
+buttonEl.addEventListener("click", countdown);
+
+
+
+
+
+
 
 // selecting answer will change screen to next question
 // underneath answers will let used know if they chose the right or wrong answer
