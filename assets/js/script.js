ceec2bee92235ef460da0ed29d1a5627dd9b913c
ashryan <20115553+ashryan125@users.noreply.github.com>
2021-05-14 21:51:36 -0400

    quiz correctly going through questions and timer is working

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,20 +34,27 @@ var correctCount = 0;
 var penalty = 10;
 // set initial timer to 75sec and first question
 var timer = 75;
+// time left on clock
+var timeLeft = 0;
 // location for quiz options/choices
 var optionListEl = document.querySelector("#quiz-choices");
 // location where "correct!" on "wrong!" displays
 var questionResultEl = document.querySelector("#question-result");
+// store a variable of setInterval
+var startCountdown;
+
+
 
 // when user clicks sumbit, start countdown timer and display first question
 document.getElementById("quiz-btn").addEventListener("click", function () {
-     var startCountdown = setInterval(function () {
+     startCountdown = setInterval(function () {
           document.getElementById("time").innerHTML = "Time: " + timer;
           timer--;
 
           if (timer === 0) {
                document.getElementById("time").innerHTML = "Time is up!";
                clearInterval(startCountdown);
+               setTimeout(quizComplete, 1000);
           }
      }, 1000);
      qDisplay(questionNum);
@@ -79,26 +86,113 @@ function qDisplay() {
 
 function nextQuestion() {
      questionNum++;
-     qDisplay();
+
+     if (questionNum >= questions.length) {
+          console.log("quiz complete");
+          clearInterval(startCountdown);
+          quizComplete();
+     } else {
+          console.log("next qustion logic");
+
+          // questionNum++;
+          console.log(questionNum);
+          console.log(questions.length);
+
+          qDisplay();
+     }
 }
 
 function answerCheck(event) {
-     if (event.target.matches("li")) {
+
+     if (event.target.matches("button")) {
           var answer = event.target.textContent;
           if (answer === questions[questionNum].answer) {
-               console.log("correct");
                questionResultEl.textContent = "Correct!";
                correctCount++;
           } else {
-               console.log("wrong");
                questionResultEl.textContent = "Wrong!";
-               // timer = timer + penalty;
+               timeLeft = timeLeft - penalty;
           }
-     }   
-     setTimeout(nextQuestion, 2000);
-     qDisplay();
+     }
+
+     setTimeout(nextQuestion, 1500);
+
 };
 
 optionListEl.addEventListener("click", answerCheck);
+
+var quizComplete = function () {
+     // clear page
+     document.getElementById("quiz-area").innerHTML = "";
+     optionListEl.innerHTML = "";
+     document.getElementById("question-result").innerHTML = "";
+
+     // create h1
+     var h1 = document.createElement("h1");
+     h1.setAttribute("id", "allDone");
+     h1.textContent = "All done!";
+
+     document.getElementById("quiz-area").appendChild(h1);
+
+     // display score
+     var finalScore = document.createElement("p");
+     finalScore.setAttribute("id", "finalScore");
+     finalScore.textContent = "Your final score is " + (correctCount + timer);
+
+     document.getElementById("quiz-area").appendChild(finalScore);
+
+     // inital input
+     var createLabel = document.createElement("label");
+     createLabel.setAttribute("id", "createLabel");
+     createLabel.textContent = "Enter your initials: ";
+
+     document.getElementById("quiz-area").appendChild(createLabel);
+
+     // input
+     var createInput = document.createElement("input");
+     createInput.setAttribute("type", "text");
+     createInput.setAttribute("id", "initials");
+     createInput.textContent = "";
+
+     document.getElementById("quiz-area").appendChild(createInput);
+
+     // submit
+     var createSubmit = document.createElement("button");
+     createSubmit.setAttribute("type", "submit");
+     createSubmit.setAttribute("id", "Submit");
+     createSubmit.setAttribute("class", "btn btn-lg quiz-start");
+     createSubmit.textContent = "Submit";
+
+     document.getElementById("quiz-area").appendChild(createSubmit);
+
+     // Event listener to capture initials and local storage for initials and score
+     createSubmit.addEventListener("click", function () {
+          var initials = createInput.value;
+
+          if (initials === null) {
+
+               console.log("No value entered!");
+
+          } else {
+               var finalScore = {
+                    initials: initials,
+                    score: timeRemaining
+               }
+               console.log(finalScore);
+               var allScores = localStorage.getItem("allScores");
+               if (allScores === null) {
+                    allScores = [];
+               } else {
+                    allScores = JSON.parse(allScores);
+               }
+               allScores.push(finalScore);
+               var newScore = JSON.stringify(allScores);
+               localStorage.setItem("allScores", newScore);
+               // Travels to final page
+               window.location.replace("./highscores.html");
+          }
+     });
+
+};
 
 
